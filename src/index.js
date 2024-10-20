99f1a163a90dc574c518ac385d569826767b8f60
Myles Gaerlan <ish.myles@gmail.com>
2024-10-20 15:42:36 +1100

    Completed basic view for pick history
    Users can view the list of picks throughout the draft

--- src/index.js
+++ src/index.js
@@ -65,3 +65,16 @@ document.addEventListener("DOMContentLoaded", () => {
     teamList.appendChild(newCard);
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const picksList = document.querySelector(".picks-list");
+
+  for (let i = 0; i < 40; i++) {
+    const pickCard = document.createElement("div");
+    pickCard.classList.add("pick-history-card");
+    pickCard.classList.add("txt-dark-grey");
+    pickCard.classList.add("txt-sm");
+    pickCard.innerHTML = `<strong>Pick #${i + 1}:</strong> ${data[i].name}`;
+    picksList.appendChild(pickCard);
+  }
+});
