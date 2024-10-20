3d5868e42688fe5723b8823477e7314aec552bac
Myles Gaerlan <ish.myles@gmail.com>
2024-10-20 15:21:15 +1100

    Created basic view for roster picks
    Users can view their list of picks in the draft

--- src/index.js
+++ src/index.js
@@ -9,7 +9,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const tableRow = document.createElement("tr");
 
     tableRow.innerHTML = `<th scope="row">${data[i].id}</th>
-      <td scope="row"><div><span class="txt-row">${data[i].name}</span><span class="txt-row"><span class="txt-sm txt-grey">${data[i].team}</span> <span class="txt-sm txt-dark-grey txt-bold">${data[i].position}</span></span></div></td>
+      <td scope="row">
+        <div class="player-details">
+          <span class="txt-row">${data[i].name}</span>
+            <span class="txt-row"><span class="txt-sm txt-grey">${data[i].team}</span> 
+            <span class="txt-sm txt-dark-grey txt-bold">${data[i].position}</span>
+          </span>
+        </div>
+      </td>
       <td>${data[i].statsPrediction.gp}</td>
       <td>${data[i].statsPrediction.fg}</td>
       <td>${data[i].statsPrediction.ft}</td>
@@ -26,4 +33,35 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   tableBody.appendChild(availablePlayers);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const teamList = document.querySelector(".team-list");
+
+  for (let i = 0; i < 14; i++) {
+    const newCard = document.createElement("div");
+    newCard.classList.add("player-card");
+    newCard.classList.add("txt-xs");
+    newCard.innerHTML = `<div>
+      <span>${data[i].name} - </span>
+      <span>${data[i].position}</span>
+    </div>
+    <table class="txt-xs">
+      <tbody>
+        <td>${data[i].statsPrediction.gp}</td>
+        <td>${data[i].statsPrediction.fg}</td>
+        <td>${data[i].statsPrediction.ft}</td>
+        <td>${data[i].statsPrediction["3pm"]}</td>
+        <td>${data[i].statsPrediction.reb}</td>
+        <td>${data[i].statsPrediction.ast}</td>
+        <td>${data[i].statsPrediction.assistTO}</td>
+        <td>${data[i].statsPrediction.stl}</td>
+        <td>${data[i].statsPrediction.blk}</td>
+        <td>${data[i].statsPrediction.to}</td>
+        <td>${data[i].statsPrediction.pts}</td>
+      </tbody>
+    </table>`;
+
+    teamList.appendChild(newCard);
+  }
 });
