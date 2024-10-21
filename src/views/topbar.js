591162cc7ca4014833e5cc404cb577fd8f4146f7
Myles Gaerlan <ish.myles@gmail.com>
2024-10-22 01:09:29 +1100

    chore: Updated styling on page
    Included app logo and updated colors & input styles

--- src/views/topbar.js
+++ src/views/topbar.js
@@ -1,10 +1,24 @@
 import pubsub from "../utils/pubsub.js";
+import icon from "../assets/images/fba-logo.png";
 
 export default function () {
   const initialise = () => {
     const topbar = document.querySelector(".topbar");
-    topbar.innerHTML = `<div></div>
+    topbar.innerHTML = `<div id="logo"></div>
                   <span class="btn-reset">RESET</span>`;
+
+    const logo = document.querySelector("#logo");
+
+    const appName = document.createElement("h1");
+    appName.textContent = "Draft Tool";
+
+    const appIcon = new Image();
+    appIcon.src = icon;
+    appIcon.width = "75";
+    appIcon.height = "42";
+
+    logo.appendChild(appIcon);
+    logo.appendChild(appName);
 
     document.querySelector(".btn-reset").addEventListener("click", () => {
       pubsub.publish("RESET");
--- webpack.common.js
+++ webpack.common.js
@@ -20,6 +20,10 @@ module.exports = {
         test: /\.css$/,
         use: [{ loader: "style-loader" }, { loader: "css-loader" }],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };
